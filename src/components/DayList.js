f3611e0c4cf7ae9556669e450a44110b92a3c681
KurtSpiker <kurt.spiker@gmail.com>
2021-10-27 03:09:17 -0600

    Button, Daylist, and Daylistitem components

---
+++ src/components/DayList.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import DayListItem from "./DayListItem";
+
+export default function DayList(props) {
+  const scheduleSpecifics = props.days.map(dayItems => {
+    return (
+      <DayListItem
+        key={dayItems.id}
+        name={dayItems.name}
+        spots={dayItems.spots}
+        selected={dayItems.name === props.day}
+        setDay={props.setDay}
+      />
+    )
+  })
+  return (
+    <ul>{scheduleSpecifics}</ul>
+  )
+}
---
+++ src/components/DayListItem.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
+
+const formatSpots = (spots) => {
+  if(!spots) {
+    return `no spots remaining`;
+  }
+  if(spots === 1) {
+    return `${spots} spot remaining`;
+  }
+
+  return `${spots} spots remaining`;
+}
+
+export default function DayListItem(props) {
+  const dayClass = classNames ("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots
+  });
+  return (
+    <li 
+      className={dayClass} 
+      onClick={() => props.setDay(props.name)}
+    >
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
